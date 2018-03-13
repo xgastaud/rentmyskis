658e9ecb04beeecbf3909b3d913d8b3f1d27e32b
come mellerio <come.mellerio@free.fr>
2018-03-13 18:34:09 +0100

    edit message string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313150155) do
+ActiveRecord::Schema.define(version: 20180313173058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180313150155) do
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "message"
+    t.text "message"
     t.index ["product_id"], name: "index_bookings_on_product_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
