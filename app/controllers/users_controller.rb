853e0d30579f322de440544fdcee4d8f2c77ee49
Emile Sautet <emilesautet@gmail.com>
2018-03-14 12:10:23 +0100

    added a functional view for dashboard owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def dashboard
     @bookings = current_user.bookings
   end
+
+  def dashboard_owner
+    @bookings = current_user.bookings
+    @requests = current_user.bookings_as_owner
+  end
 end
