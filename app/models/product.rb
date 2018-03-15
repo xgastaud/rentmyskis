984c8a88cdbd20bde372dfcd58cc70f6e589b7db
Emile Sautet <emilesautet@gmail.com>
2018-03-15 14:49:34 +0100

    almost finished the index page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,12 @@ class Product < ApplicationRecord
   has_many :bookings
   validates :title, :category, :price_per_day, :location, :description, presence: true
   mount_uploader :photo, PhotoUploader
+
+  CATEGORY_COLORS = {
+    "Skis" => "#DAA520",
+    "Snowboard" => "#005792",
+    "Other" => "#307672"
+  }
+
+  validates :category, inclusion: { in: CATEGORY_COLORS.keys, message: "Select a category" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,26 +12,26 @@ xavier.save
 emile = User.new(email: "emilesautet@gmail.com", password: "azerty", username: "emilesautet")
 emile.save
 
-product1 = Product.new(title: "skis salomon 175", category: "skis", price_per_day: 35.3, location: "Tignes", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product1 = Product.new(title: "skis salomon 175", category: "Skis", price_per_day: 35.3, location: "Tignes", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product1.user = xavier
 product1.save
 
-product2 = Product.new(title: "skis rossignol 185", category: "skis", price_per_day: 41, location: "Val d'Isère", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product2 = Product.new(title: "skis rossignol 185", category: "Skis", price_per_day: 41, location: "Val d'Isère", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product2.user = emile
 product2.save
 
-product3 = Product.new(title: "surf oxbow 120", category: "surf", price_per_day: 23, location: "Chamonix", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product3 = Product.new(title: "surf oxbow 120", category: "Snowboard", price_per_day: 23, location: "Chamonix", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product3.user = emile
 product3.save
 
-product4 = Product.new(title: "surf quicksilver 100", category: "surf", price_per_day: 27, location: "Les Arcs", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product4 = Product.new(title: "surf quicksilver 100", category: "Snowboard", price_per_day: 27, location: "Les Arcs", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product4.user = xavier
 product4.save
 
-product5 = Product.new(title: "surf Rossignol enfant", category: "surf", price_per_day: 40, location: "Valmorel", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product5 = Product.new(title: "surf Rossignol enfant", category: "Snowboard", price_per_day: 40, location: "Valmorel", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product5.user = emile
 product5.save
 
-product6 = Product.new(title: "surf Head 130", category: "surf", price_per_day: 10, location: "Argentiere", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
+product6 = Product.new(title: "surf Head 130", category: "Other", price_per_day: 10, location: "Argentiere", description: "The best pair of skis ever!!!!!!!!!!!!!!!!")
 product6.user = xavier
 product6.save
