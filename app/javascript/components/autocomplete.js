d9d5b14b54873d863b3011021e1aa5fd02bf2307
Xavier Gastaud <xgastaud@gmail.com>
2018-03-15 15:02:44 +0100

    search bar with autocomplete on home and new

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,11 +1,12 @@
 // app/javascript/components/autocomplete.js
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var flatAddress = document.getElementById('flat_address');
+    var productAddress = document.querySelector(".autocomplete");
+    // var productAddress = document.getElementById('address');
 
-    if (flatAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
+    if (productAddress) {
+      var autocomplete = new google.maps.places.Autocomplete(productAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(productAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
