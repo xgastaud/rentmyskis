dd21ba6e5f85f84cbb48c2108d55e018adde74fa
Xavier Gastaud <xgastaud@gmail.com>
2018-03-15 19:20:09 +0100

    reinstated links to change status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :product
   validates :starts_at, presence: true
   validates :ends_at, presence: true
-  validates :status, inclusion: { in: ["accepted", "pending", "rejected"], allow_blank: false }
+  validates :status, inclusion: { in: ["Accepted", "Pending", "Rejected"], allow_blank: false }
 end
